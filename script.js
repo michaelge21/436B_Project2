4927a4fde856f0af7f6c6d432ceefd73c13de8ad
Michael Ge <michaelge@wustl.edu>
2022-02-20 14:59:42 -0600

    bruhh

--- script.js
+++ script.js
@@ -5,4 +5,7 @@ function zoomClick() {
 function remove() {
     var element = document.getElementById("disappear");
     element.remove();
+
+    var friend = document.getElementById("friend");
+    friend.innerHTML = "bro did... did you just delete my friend?"
 }
