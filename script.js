f5abc6fe41a98623f4c209db8f54cd5c49be2a52
Michael Ge <michaelge@wustl.edu>
2022-02-20 21:21:53 -0600

    i give up for the night

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ function remove() {
     friend.innerHTML = "bro did... did you just delete my friend?"
     friend.style.backgroundImage = "url('Sad_Face_Emoji_large.png')";
 }
+
+function add() {
+    var addElement = document.createElement("a");
+    addElement.innerHTML = "woooooooo";
+    addElement.style.display = "block";
+    addElement.style.textAlign = "center";
+    addElement.style.margin = "0";
+    addElement.style.textDecoration = "none";
+    addElement.style.transition = "1s all";
+    addElement.style.borderRadius = "5px";
+    addElement.style.padding = "10px";
+    addElement.style.backgroundColor = "rgba(252, 252, 252, 0.678)";
+    addElement.style.border = "2px solid rgb(79,185,227)";
+    addElement.style.textAlign = "center";
+    addElement.style.margin = "auto";
+    
+
+
+    document.body.appendChild(addElement);
+}
